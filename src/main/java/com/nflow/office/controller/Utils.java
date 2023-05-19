6e44614395dc2a6b75be55ea05adee6f0523a3f8
AC931756 <jmkrose0704@gmail.com>
2023-05-19 14:58:41 +0900

    csv파일 경로 변경

--- src/main/java/com/nflow/office/controller/Utils.java
+++ src/main/java/com/nflow/office/controller/Utils.java
@@ -15,8 +15,8 @@ import java.util.List;
 
 public class Utils {
 	public List<List<String>> readCsvFile() throws IOException {
-		//String sourceFile = "/home/ec2-user/myproject/hostlist.csv";
-		String sourceFile = "D://test/hostlist.csv";
+		String sourceFile = "/home/ec2-user/myproject/hostlist.csv";
+		//String sourceFile = "D://test/hostlist.csv";
 		List<List<String>> csvList = new ArrayList<>();
 		File csv = new File(sourceFile);
 		BufferedReader br = null;
@@ -38,8 +38,8 @@ public class Utils {
 		  
 	public void writeCsvFile(List<String> resultList) throws IOException {
 		LocalDateTime now = LocalDateTime.now();
-		//String targetFile = "/home/ec2-user/myproject/telnetResult_" + now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + ".csv";
-		String targetFile = "D://test/telnetResult_" + now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + ".csv";
+		String targetFile = "/home/ec2-user/myproject/telnetResult_" + now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + ".csv";
+		//String targetFile = "D://test/telnetResult_" + now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + ".csv";
 		File resultCsv = new File(targetFile);
 		BufferedWriter bw = null;
 		bw = new BufferedWriter(new FileWriter(resultCsv, false));
